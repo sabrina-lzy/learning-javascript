99f3e1b6d27dae93884e177d369263fa34ca717d
sabrina-lzy <sabrinaleezhiying95.16@ichat.sp.edu.sg>
2018-06-14 16:48:38 +0800

    Create looping.js
    Added a For Loop and While Loop Examples

---
+++ looping.js
@@ -0,0 +1,21 @@
+// For Loop Examples
+var count = 0;
+for (var i = 0; i < 5; i++) {
+ count = count + i;
+}
+
+
+for (var berries = 5; berries > 0; berries--) {
+ alert("Eating a berry");
+}
+
+
+
+// While Loop Examples
+var tops = 5;
+while (tops > 0) {
+ for (var spins = 0; spins < 3; spins++) {
+ alert("Top is spinning!");
+ }
+ tops = tops - 1;
+}
